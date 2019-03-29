b25a85e00316d83fe907d203dd35db92fc382880
karol <karol@karol-pc-debian.kompikownia>
2019-03-29 16:33:06 +0100

    First commit

---
+++ src/main/java/pl/kompikownia/dziennik/dziennik/DziennikApplication.java
@@ -0,0 +1,13 @@
+package pl.kompikownia.dziennik.dziennik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DziennikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DziennikApplication.class, args);
+	}
+
+}
